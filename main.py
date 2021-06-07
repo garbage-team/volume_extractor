68b2f8d4d91bdfb9f7f7e70acca6e685241847ec
hanefors <hanefors@kth.se>
2021-06-07 12:59:00 +0200

    Update main.py
    Updated main.py to run on the RPi and show the fill rate as a text on the screen, with different colors for empty/full. Small fixes to make it work.
    
    Note that script is still very messy and should/could be cleaned up, but works for now.

--- main.py
+++ main.py
@@ -7,24 +7,43 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
 
-MODEL_PATH = "./lite_model_wo_frl.tflite"
+MODEL_PATH = "./lite_model_w_frl.tflite"
 
 def main():
-    intel = (69.4, 42.5)
+    cam_fov = (82.1, 52.2)
+    vol_empty = -1.231
+    fig, ax = plt.subplots(1,1)
+    fig.suptitle('Garbage bin fill rate')
+    plt.axis('off')
+    img = ax.imshow(np.zeros((224,224)))
+    img_text = ax.text(50,50,'Measuring..', color='red', fontsize=60)
     while True:
         rgb, depth = run_model(MODEL_PATH)
         depth = depth[0]
-        xyz = depth_to_xyz(depth, fov=intel)
-        xyz_clipped = clip_by_border(xyz, (-.9, .9), (-.35, .35), (2.4, 1))
+        rgb = rgb[0]
+        xyz = depth_to_xyz(depth, fov=cam_fov)
+        xyz_clipped = clip_by_border(xyz, (-1.4, 1.4), (-0.5, 0.75), (2.4, 1))
         xyz_clipped[:, 2] = 1.5 - xyz_clipped[:, 2]
         volume = xyz_to_volume(xyz_clipped)
-
+        fill_rate = (1 - round(volume,3)/vol_empty)
+        fill_rate_prc = round(fill_rate*100)
         print("Volume in container: ", volume, "m^3")
-
-        # display_rgbd([rgb, depth_empty])
-        display_point_cloud(xyz_clipped)
+        display_image(fig, img, img_text, rgb, fill_rate_prc)
+       	display_point_cloud(xyz_clipped)
     return None
 
+
+def display_image(fig, im, text, rgb, fill_rate):
+    if fill_rate < 75:
+        txt_color = '#3ad20b' # Green if not full
+    else:
+        txt_color = 'red' # Red if needs to be emptied
+    im.set_data(rgb)
+    text.set_text(str(fill_rate)+"%")
+    text.set_color(txt_color)
+    fig.canvas.draw_idle()
+    plt.pause(1)
+    return None
 
 def display_point_cloud(xyz):
     fig = plt.figure()
