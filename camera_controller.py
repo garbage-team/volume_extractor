cc5a0ef95cf03d9f90cb78571cf94df552c34a18
Jungåker Jonas <jonasjungaker@gmail.com>
2021-06-09 12:35:44 +0200

    refactor camera controller to CameraInterface class

--- camera_controller.py
+++ camera_controller.py
@@ -3,26 +3,37 @@ import numpy as np
 from math import pi
 
 
-def camera_capture(output_size=(224, 224), normalize=True, video_channel=0, crop_fov=(69.4, 42.5)):
-    cap = cv2.VideoCapture(video_channel)
-    ret, frame = cap.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = crop_to_fov(frame, crop_fov)
-    frame = cv2.resize(frame, output_size)
-    if normalize:
-        frame = frame.astype('float32')
-        frame = frame / 255.0
-    return frame
+class CameraInterface:
+    def __init__(self, input_fov, output_fov, output_size, video_channel=0):
+        self.capture_stream = cv2.VideoCapture(video_channel)
+        self.input_fov = input_fov
+        self.output_fov = output_fov
+        self.output_size = output_size
 
+    def capture_image(self, crop_to_fov=True, normalize=True, empty_buffer=True):
+        if empty_buffer:
+            for i in range(10):  # Empty capture buffer, get fresh image
+                _, _ = self.capture_stream.read()
+                cv2.waitKey(1)
+        ret, frame = self.capture_stream.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        if crop_to_fov:     # Narrow the field of view of the image
+            frame = self._crop_to_fov(frame)
+        if normalize:       # Divide all the pixel values with 255 to normalize
+            frame = frame.astype("float32")  # Convert from uint8 to float32
+            frame = frame / 255.
+        # Finally resize the image to given size
+        frame = cv2.resize(frame, self.output_size)
+        return frame
 
-def crop_to_fov(frame, fov):
-    x_fov, x_in_fov = fov[0], 82.1
-    y_fov, y_in_fov = fov[1], 52.2
-    x_factor = np.sin((x_fov * pi) / 180) / np.sin((x_in_fov * pi) / 180)
-    y_factor = np.sin((y_fov * pi) / 180) / np.sin((y_in_fov * pi) / 180)
-    new_size = (int(frame.shape[0] * x_factor), int(frame.shape[1] * y_factor))
-    offset_x = (frame.shape[0] - new_size[0]) // 2
-    offset_y = (frame.shape[1] - new_size[1]) // 2
-    out_frame = frame[offset_x:(offset_x + new_size[0]),
-                      offset_y:(offset_y + new_size[1])]
-    return out_frame
+    def _crop_to_fov(self, frame):
+        x_fov, x_in_fov = self.output_fov[0], self.input_fov[0]
+        y_fov, y_in_fov = self.output_fov[1], self.input_fov[1]
+        x_factor = np.sin((x_fov * pi) / 180) / np.sin((x_in_fov * pi) / 180)
+        y_factor = np.sin((y_fov * pi) / 180) / np.sin((y_in_fov * pi) / 180)
+        new_size = (int(frame.shape[0] * x_factor), int(frame.shape[1] * y_factor))
+        offset_x = (frame.shape[0] - new_size[0]) // 2
+        offset_y = (frame.shape[1] - new_size[1]) // 2
+        out_frame = frame[offset_x:(offset_x + new_size[0]),
+                          offset_y:(offset_y + new_size[1])]
+        return out_frame
